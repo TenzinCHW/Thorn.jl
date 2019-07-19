40222c344b2b680427b832cfc191537736c12776
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-07-19 20:32:11 +0800

    Added SpikeQueue and made type of time_delta an AbstractFloat

--- src/spike.jl
+++ src/spike.jl
@@ -1,5 +1,11 @@
-struct Spike
-    neuron::Int
-    time::Float64
+# Spikes are events; all their charge is delivered in an instant (like a Dirac-delta function).
+
+struct Spike{T<:AbstractFloat}
+    neuron_index::Int # Global index of neuron that fired this spike
+    time_delta::T # Time since preceding spike
+end
+
+struct SpikeQueue
+    spikes::Array{Spike, 1}
 end
 
