6c8dc9c5dd06bf2cd9753852adc82dfbb7fb0fea
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-07-21 21:18:12 +0800

    Added incomplete function signatures and comments to guide implementation

--- src/structure/cortex.jl
+++ src/structure/cortex.jl
@@ -1,2 +1,24 @@
-struct Cortex
+struct Cortex{T<:AbstractFloat}
+    input_populations::Array{InputNeuronPopulation, 1}
+    populations::Array{NeuronPopulation, 1}
+    pop_connectivity_matrix::Array{Bool, 2}
+    interpop_weights::Array{Union{Nothing, Array{T, 2}}, 2}
+end
+
+function generate_input_spikes()
+    # TODO Given a set of raw sensor data and a Cortex, generate input spikes using the generate_static_input function of each corresponding InputNeuronPopulation
+    # Length of array of raw data must match corresponding InputNeuronPopulation
+    # If raw sensor data is 2D, the first dimension must represent space and second is time
+    # Call generate_static_input for each vector of inputs in second dim, broadcast across the first dimension
+    # Must sort the out_spikes property of each InputNeuronPopulation after generating the spikes
+end
+
+function process_sample()
+    # TODO While any population in a Cortex has spikes in its out_spikes property, call process_next_spike
+end
+
+function process_next_spike(cortex::Cortex)
+    # TODO Take the head spike from out_spikes queue of each population with smallest time property
+    # Route this spike to the correct populations with their weights using the process_spike function for every population that the spike is routed to
+    # Call output_spikes to generate output spikes for each of those populations
 end
--- src/structure/population.jl
+++ src/structure/population.jl
@@ -1,11 +1,30 @@
 include("../spike.jl")
 include("../neurons/neuron.jl")
 
-struct NeuronPopulation{S<:AbstractFloat, T<:AbstractFloat}
-    neurons::Array{Neuron, 1}
+abstract type NeuronPopulation end
+
+mutable struct ProcessingNeuronPopulation{S<:AbstractFloat, T<:AbstractFloat} <: NeuronPopulation
+    neurons::Array{ProcessingNeuron, 1}
     potential_function::Function # This is the general solution to the membrane potential DE
-    weights::Array{S, 2}
+    intrapop_weights::Array{S, 2}
     weight_update::Function
+    out_spikes::Array{Spike, 1}
+    last_spike::Spike
+end
+
+function process_spike()
+    # TODO Given a population, an input spike and the corresponding weights, compute new state of each neuron in pop
+    # Assign the spike to the last_spike variable of the pop
+end
+
+function output_spikes()
+    # TODO Compute a list of spikes output and only insert those that spike before the next spike in out_spikes array of the pop into that same array
+    # Update the given weights
+end
+
+struct InputNeuronPopulation <: NeuronPopulation
+    neurons::Array{InputNeuron, 1}
+    generate_static_input::Function # Should take in a raw sensor input and an InputNeuron and gnerate output spikes, putting them into out_spikes
     out_spikes::Array{Spike, 1}
 end
 
