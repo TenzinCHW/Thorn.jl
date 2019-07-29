8f67af11a67bd2e9f621a1f169e494cdcb60c9aa
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-07-29 21:30:54 +0800

    Removed SpikeQueue struct and added LIFSpike as subtype of Spike

--- src/spike.jl
+++ src/spike.jl
@@ -5,9 +5,15 @@ abstract type Spike end
 struct LIFSpike{T<:AbstractFloat}
     neuron_index::UInt # Localindex of neuron that fired this spike
     time::T # Global time in ms since start of simulation
-end
 
-struct SpikeQueue
-    spikes::Array{Spike, 1}
+    function LIFSpike(neuron_index::UInt, time::T) where T<:AbstractFloat
+        if (neuron_index < 1)
+            error("neuron_index must be and Int > 0")
+        end
+        if (time < 0)
+            error("time must be <:AbstractFloat >= 0")
+        end
+        new{typeof(time)}(neuron_index, time)
+    end
 end
 
