f9d6581bc5c1b88a481f13971ac8c287226e4b8e
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-07-29 21:45:32 +0800

    Complete NeuronPopulation implementations

--- src/structure/population.jl
+++ src/structure/population.jl
@@ -12,9 +12,12 @@ mutable struct ProcessingNeuronPopulation{S<:AbstractFloat, T<:AbstractFloat} <:
     out_spikes::Array{Spike, 1}
     last_spike::Spike
 
-    function ProcessingNeuronPopulation(id::UInt, neuron_type::DataType, sz::UInt, state_update::Function, output_spike::Function, weight_update::Function)
-        neurons = [neuron_type(i) for i in 1:sz]
-        new(id, neurons, state_update, output_spike, weight_update, Array{Spike, 1}, nothing)
+    function ProcessingNeuronPopulation(id::Int, neuron_type::Module, sz::Int, weight_update::Function)
+        if (id < 1 || sz < 1)
+            error("sz and id must be > 0")
+        end
+        neurons = [neuron_type.NeuronStruct(i) for i in 1:sz]
+        new(UInt(id), neurons, neuron_type.state_update, neuron_type.output_spike, weight_update, Array{Spike, 1}, nothing)
     end
 end
 
@@ -27,7 +30,7 @@ end
 function output_spike!(pop::NeuronPopulation, next_spike::Spike)
     new_spikes = flatten(pop.output_spike.(pop.neurons, next_spike))
     # filter this based on the timing of next_spike coming into the pop then sort the filtered array
-    for s in sort(filter!(x->x!=nothing, new_spikes), rev=true)
+    for s in sort(filter!(x->x!=nothing, new_spikes), by=x->x.time, rev=true)
         push!(pop.out_spikes, s) # This is faster than concat by 8x
     end
 end
@@ -53,10 +56,21 @@ function update_weights!(pop::NeuronPopulation, weights::Array{AbstractFloat, 1}
 end
 
 struct InputNeuronPopulation <: NeuronPopulation
+    id::UInt
     neurons::Array{InputNeuron, 1}
-    generate_static_input::Function # Should take in a raw sensor input and an InputNeuron and gnerate output spikes, putting them into out_spikes
+    generate_func::Function # Should take in a raw sensor input and an InputNeuron and gnerate output spikes, putting them into out_spikes
     out_spikes::Array{Spike, 1}
-end
 
-# TODO write function for generate_state_input
+    function InputNeuronPopulation(id::Int, neuron_type::Module, sz::Int)
+        if (id < 1 || sz < 1)
+            error("id and sz must be > 0")
+        end
+        if !(neuron_type<:InputNeuron)
+            error("neuron_type must be a type of InputNeuron")
+        end
+
+        neurons = [neuron_type.NeuronStruct() for i in 1:sz] # TODO Need the syntax for init an InputNeuron
+        new(UInt(id), neurons, neuron_type.generate_input, Array{Spike, 1})
+    end
+end
 
