79999ae8118117f7b0e936704b890c82d72a5c65
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-06 16:46:31 +0800

    Moved constructor method inside struct

--- src/neurons/LIF.jl
+++ src/neurons/LIF.jl
@@ -1,5 +1,5 @@
-module LIFNeuron
-    include("neuron.jl")
+module LIF
+    include("../neuron.jl")
     include("../spike.jl")
 
     mutable struct NeuronStruct{T<:AbstractFloat} <: ProcessingNeuron
@@ -13,9 +13,11 @@ module LIFNeuron
         resist::T
         last_out::Spike
 
-        function NeuronStruct(id::Int, u::T, rest_u::T, reset_u::T tau::T, thresh::T, resist::T) where T<:AbstractFloat
-            new(UInt(id), u, rest_u, reset_u, tau, thresh, resist, nothing)
+        function NeuronStruct(id::Int, u::T, rest_u::T, reset_u::T, alpha::T, tau::T, thresh::T, resist::T) where T<:AbstractFloat
+            new{typeof(u)}(UInt(id), u, rest_u, reset_u, alpha, tau, thresh, resist, nothing)
         end
+
+        NeuronStruct(id::Int) = NeuronStruct(UInt(id), def_u, def_rest_u, def_reset_u, def_alpha, def_tau, def_thresh, def_resist)
     end
 
     # TODO Find good default values for reset_u, tau, thresh and resist
@@ -26,7 +28,6 @@ module LIFNeuron
     def_tau = 1
     def_thresh = 5
     def_resist = 0.5
-    NeuronStruct(id::Int) = NeuronStruct(id, def_u, def_rest_u, def_reset_u, def_alpha, def_tau, def_thresh, def_resist, nothing)
 
     function state_update(neuron::NeuronStruct, weight::AbstractFloat, spike::Spike, prev_spike::Spike)
         decayed = e ^ - (spike.time - prev_spike.time)
