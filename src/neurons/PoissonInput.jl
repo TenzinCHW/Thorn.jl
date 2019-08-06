26d2f6873069d730df01d6c9c7f32062d93ead1e
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-06 16:46:00 +0800

    Added implementation for Poisson input

--- src/neurons/PoissonInput.jl
+++ src/neurons/PoissonInput.jl
@@ -1,5 +1,6 @@
 module PoissonInput
-    include("neuron.jl")
+    include("../neuron.jl")
+    include("../spike.jl")
 
     # Rates are in Hz
     maxrate = 20.
@@ -7,21 +8,33 @@ module PoissonInput
     sampleperiod = 1000. # Milliseconds
 
     struct NeuronStruct{T<:AbstractFloat} <: InputNeuron
+        id::UInt
         maxrate::T
         minrate::T
         sampleperiod::T
 
-        function NeuronStruct()
-            new{typeof(maxrate)}(maxrate, minrate, sampleperiod)
+        function NeuronStruct(id::Int)
+            NeuronStruct(UInt(id), maxrate, minrate, sampleperiod)
         end
 
-        function NeuronStruct(maxrate, minrate, sampleperiod)
-            new{typeof(maxrate)}(maxrate, minrate, sampleperiod)
+        function NeuronStruct(id::Int, maxrate, minrate, sampleperiod)
+            new{typeof(maxrate)}(UInt(id), maxrate, minrate, sampleperiod)
         end
     end
 
-    function generate_input(neuron::NeuronStruct{T}, sensor_inp::T) where T
+    function generate_input(neuron::NeuronStruct{T}, maxval::T, sensor_inp::T, spiketype) where T<:AbstractFloat
+        time = 0
+        spikes = Array{Spike, 1}
+        rate = compute_rate(maxval, sensor_inp)
+        while (time < neuron.sampleperiod)
+            dt = exprand(rate)
+            time += dt
+            push!(spikes, spiketype(neuron.id, dt))
+        end
+        spikes
     end
 
-    export NeuronStruct
+    compute_rate(maxval, val) = val / maxval * (maxrate - minrate) + minrate
+
+    exprand(rate) = - log(1 - rand()) / rate
 end
