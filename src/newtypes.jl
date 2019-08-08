fbb0350930edbfa71c3318c7bd6084865cf650b6
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-08 23:24:58 +0800

    Added macros for importing functions for defining new input neurons, new population behaviours and new cortex behaviours

--- src/newtypes.jl
+++ src/newtypes.jl
@@ -1,3 +1,20 @@
-macro newneuron()
+macro newprocessingneuron()
     ex = :($(Expr(:toplevel, :(import SpikingNN.state_update!), :(import SpikingNN.output_spike!))))
 end
+
+macro newinputneuron()
+    ex = :($(Expr(:toplevel, :(import SpikingNN.generate_input))))
+end
+
+macro newprocpop()
+    ex = :($(Expr(:toplevel, :(import SpikingNN.process_spike!))))
+end
+
+macro newinppop()
+    ex = :($(Expr(:toplevel, :(import SpikingNN.generate_input_spikes!))))
+end
+
+macro newcortex()
+    ex = :($(Expr(:toplevel, :(import SpikingNN.process_sample!))))
+end
+
