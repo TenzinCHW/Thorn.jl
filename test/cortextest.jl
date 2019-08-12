bfab7d960ff2553d91e9415abf3b8ef22c091fd3
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-12 21:27:41 +0800

    Fixed connection matrix

--- test/cortextest.jl
+++ test/cortextest.jl
@@ -1,7 +1,7 @@
 input_neuron_types = [(PoissonInpNeuron, 5)]
 neuron_types = [(LIFNeuron, 5, stdp, 0.01)]
-conn = BitArray([[0 1];
-                 [0 0]])
+conn = BitArray([[0 0];
+                 [1 0]])
 spiketype = LIFSpike
 cortex = Cortex(input_neuron_types, neuron_types, conn, spiketype)
 data = [rand(sz)]
