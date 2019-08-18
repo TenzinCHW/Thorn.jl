7cc81fc18803e42d7b626e22a2797efb9e6fd2e1
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-18 14:27:27 +0800

    Replaced connectivity matrix with Dict according to new API

--- test/cortextest.jl
+++ test/cortextest.jl
@@ -1,7 +1,6 @@
 input_neuron_types = [(PoissonInpNeuron, 5)]
 neuron_types = [(LIFNeuron, 5, stdp, 0.01)]
-conn = BitArray([[0 0];
-                 [1 0]])
+conn = Dict(1=>2)
 spiketype = LIFSpike
 cortex = Cortex(input_neuron_types, neuron_types, conn, spiketype)
 data = [rand(sz)]
