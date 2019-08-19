a9e6220e498d70c2fd86ea40b83205488b6f7d33
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-20 01:39:41 +0800

    Added functionality to filter nothing values that are recorded during monitoring

--- src/monitor/monitor.jl
+++ src/monitor/monitor.jl
@@ -1,16 +1,6 @@
-#struct Monitor
-#    extractors::Dict{String, Function}
-#    data::Dict{String, Array}
-#
-#    function Monitor(extractors::Dict{String, Function})
-#        data = Dict(name=>[] for (name, _) in extractors)
-#        new(extractors, data)
-#    end
-#end
-
-function monitor(cortex::Cortex, extractors::Dict, data::Dict{String, Array{Dict, 1}})
+function monitor!(cortex::Cortex, pop_spike::Tuple{UInt, S}, extractors::Dict, data::Dict{String, Array{Dict, 1}}) where S<:Spike
     for (name, extractor) in extractors
-        push!(data[name], deepcopy(extractor(cortex)))
+        push!(data[name], deepcopy(extractor(cortex, pop_spike)))
     end
 end
 
@@ -18,7 +8,7 @@ function collapse(arr::Array{Dict, 1})
     ks = collect(keys(first(arr)))
     result = Dict(key=>[] for key in ks)
     for t in arr
-        map(k->push!(result[k], t[k]), ks)
+        map(k->push!(result[k], t[k]), filter(k->!isnothing(t[k]), ks))
     end
     collapse(result)
 end
