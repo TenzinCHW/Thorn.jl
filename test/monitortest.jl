8641684a0c48c8582968929f5c1ae9a28e26ef9f
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-19 20:20:03 +0800

    Made test more readable

--- test/monitortest.jl
+++ test/monitortest.jl
@@ -1,5 +1,6 @@
 extractors = Dict("weights"=>c->c.weights,
-                  "u"=>c->Dict(pop.id=>[n.u for n in pop.neurons] for pop in c.processing_populations))
+                  "u"=>c->Dict(pop.id=>[n.u for n in pop.neurons]
+                               for pop in c.processing_populations))
 
 input_neuron_types = [(PoissonInpNeuron, 5)]
 neuron_types = [(LIFNeuron, 5, stdp, 3.)]
@@ -12,6 +13,8 @@ data = [rand(sz)]
 _, monitor = process_sample!(cortex, data, 1., extractors)
 
 import InteractiveUtils
-@test any(isa.([collapse(monitor["weights"])[1=>2]], [Array{T, 3} for T in InteractiveUtils.subtypes(AbstractFloat)]))
-@test any(isa.([collapse(monitor["u"])[2]], [Array{T, 2} for T in InteractiveUtils.subtypes(AbstractFloat)]))
+weights = collapse(monitor["weights"])[1=>2]
+u = collapse(monitor["u"])[2]
+@test any(isa.([weights], [Array{T, 3} for T in InteractiveUtils.subtypes(AbstractFloat)]))
+@test any(isa.([u], [Array{T, 2} for T in InteractiveUtils.subtypes(AbstractFloat)]))
 
