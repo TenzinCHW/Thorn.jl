b12874f442c9c9300ca3dbb3b32a1214c41d1479
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-20 16:31:56 +0800

    Added homeostasis and modified default values of parameters

--- src/neurons/LIF.jl
+++ src/neurons/LIF.jl
@@ -6,23 +6,27 @@ mutable struct LIFNeuron{T<:AbstractFloat} <: ProcessingNeuron
     alpha::T
     tau::T
     thresh::T
+    homeostasis_a::T
+    homeostasis_b::T
     last_out::Union{Spike, Nothing}
     u_func::Function # u as a function of time diff between two spikes
 
-    function LIFNeuron(id::Int, u::T, rest_u::T, reset_u::T, alpha::T, tau::T, thresh::T, u_func) where T<:AbstractFloat
-        new{typeof(u)}(UInt(id), u, rest_u, reset_u, alpha, tau, thresh, nothing, u_func)
+    function LIFNeuron(id::Int, u=def_u, rest_u=def_rest_u, spike_u=def_spike_u, alpha=def_alpha, tau=def_tau, thresh=def_thresh, homeostasis_a=def_homeostasis_a, homeostasis_b=def_homeostasis_b, u_func=dt->def_u)
+        new{typeof(u)}(UInt(id), u, rest_u, spike_u, alpha, tau, thresh, homeostasis_a, homeostasis_b, nothing, u_func)
     end
 
-    LIFNeuron(id::Int) = LIFNeuron(id, def_u, def_rest_u, def_spike_u, def_alpha, def_tau, def_thresh, dt->def_u)
+    #LIFNeuron(id::Int) = LIFNeuron(id, def_u, def_rest_u, def_spike_u, def_alpha, def_tau, def_thresh, dt->def_u)
 end
 
 # TODO Find good default values for reset_u, tau and thresh
 def_u = 0.
 def_rest_u = 0.
 def_spike_u = -5.
-def_alpha = 1.
-def_tau = 1.
+def_alpha = .8
+def_tau = 16.8
 def_thresh = 5.
+def_homeostasis_a = 0.1
+def_homeostasis_b = 0.01
 
 function state_update!(neuron::LIFNeuron, weight::AbstractFloat, spike::S, prev_spike::Union{S, Nothing}) where S<:Spike
     prev_spike_t = isnothing(prev_spike) ? 0. : prev_spike.time
@@ -34,6 +38,7 @@ end
 function output_spike!(neuron::LIFNeuron, spike::T, next_spike::Union{T, Nothing}) where T<:Spike
     if (neuron.u >= neuron.thresh && (isnothing(next_spike) || spike.time <= next_spike.time))
         neuron.u = neuron.spike_u
+        neuron.thresh += exp(neuron.homeostasis_a * neuron.thresh + neuron.homeostasis_b)
         neuron.last_out = LIFSpike(neuron.id, spike.time)
     end
 end
