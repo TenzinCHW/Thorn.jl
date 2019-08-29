4cce7b688fda7f10c27739dbac042260edc92ba9
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-29 22:42:39 +0800

    Added code to ensure number of datapoints is correct

--- src/monitor/plotting.jl
+++ src/monitor/plotting.jl
@@ -3,7 +3,7 @@ function rasterspikes(spikes::Array{Tuple{Int, T}, 1}, cortex::Cortex) where T<:
     dy = 1
     x = []
     y = []
-    for (i, num) in enumerate(length.(cortex.populations))
+    for (i, num) in enumerate(pop.length for pop in cortex.populations)
         sp = filter(x->first(x) == i, spikes)
         xs = [last(s).time for s in sp]
         ys = [currentht + last(s).neuron_index * dy for s in sp]
@@ -14,21 +14,31 @@ function rasterspikes(spikes::Array{Tuple{Int, T}, 1}, cortex::Cortex) where T<:
     x, y
 end
 
-function gridify(val::Array{T, 1}, diffeq, spikes::Array{S, 1}, dt::T, time_end::T) where {T<:AbstractFloat, S<:Spike, A}
+function gridify(val::Array{T, 1}, diffeq, spikes::Array{S, 1}, dt::T, time_end::T;
+                 init_val::T=0.) where {T<:AbstractFloat, S<:Spike, A}
     # val is an array of initial values of the diffeq corresponding to the times of the spikes
     !isequal(length.([val, spikes])...) ? error("val should be same length as spikes") : nothing
     isfloat.(val) # Should not throw error
     x = makegrid(dt, time_end)
     y = T[]
-    start = 0
+    start = 0.
     for (s, v) in zip(spikes, val)
-        dy = diffeq.(v, filter(dx->start<=dx<s.time, x) .- start)
+        interpolate(x, y, diffeq, init_val, start, s.time)
+        init_val = v
         start = s.time
-        for ddy in dy
-            push!(y, ddy)
-        end
     end
+    # need the initial value when the last spike occurred
+    lastval = diffeq(init_val, spikes[end].time - spikes[end-1].time)
+    interpolate(x, y, diffeq, lastval, start, time_end + dt)
     x, y
+end
+
+function interpolate(x::Array{T, 1}, y::Array{T, 1}, f::Function, u0::T,
+                     start::T, time_end::T) where T<:AbstractFloat
+    dy = f.(u0, filter(dx->start<=dx<time_end, x) .- start)
+    for ddy in dy
+        push!(y, ddy)
+    end
 end
 
 function isfloat(item::AbstractFloat)
