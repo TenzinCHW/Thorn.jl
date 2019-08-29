7462bb1826eb8ab90f68f5a012e129864f0a820e
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-08-29 13:26:35 +0800

    Fixed rate computation

--- src/learning/stdp.jl
+++ src/learning/stdp.jl
@@ -2,16 +2,16 @@
 # Should I apply stdp for every pair of input and output spikes or just the last input and last output spike?
 # If I do just last input and last output, I just have to apply a weight update before processing each input spike and after each output spike
 function stdp(pop::NeuronPopulation, w::T, last_out::Union{Spike, Nothing}, prev_spike::Spike, next_spike::Union{Spike, Nothing}) where T<:AbstractFloat
-    alpha, lr, tau = pop.alpha, pop.lr, pop.tau
+    α, η, τ = pop.α, pop.η, pop.τ
     if !isnothing(last_out) && !isnothing(next_spike)
         # Update based on time difference between prev_spike and neuron.last_out
-        weaken = - alpha * exponent(last_out.time, next_spike.time, tau)
+        weaken = - α * exponent(last_out.time, next_spike.time, τ)
         # Update based on time difference between neuron.last_out and next_spike
-        strengthen = exponent(-prev_spike.time, -last_out.time, tau)
-        return lr * (strengthen + weaken)
+        strengthen = exponent(-prev_spike.time, -last_out.time, τ)
+        return η * (strengthen + weaken)
     end
     w
 end
 
-exponent(pre::T, post::T, tau::T) where T<:AbstractFloat = exp((pre - post) / tau)
+exponent(pre::T, post::T, τ::T) where T<:AbstractFloat = exp((pre - post) / τ)
 
--- src/neurons/Input.jl
+++ src/neurons/Input.jl
@@ -1,2 +1,4 @@
-compute_rate(pop::InputPopulation, maxval, val) = (val / maxval * (pop.maxrate - pop.minrate) + pop.minrate) / pop.sampleperiod
+ms = .001
+
+compute_rate(pop::InputPopulation, maxval, val) = (val / maxval * (pop.maxrate - pop.minrate) + pop.minrate) * ms
 
