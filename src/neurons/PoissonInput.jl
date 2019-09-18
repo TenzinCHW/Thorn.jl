13a5f8ce28b90ac0775e4c0bc7df5e82664446c4
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-09-18 17:59:23 +0800

    PoissonInput population produces spikes for lengths of time corresponding to number of timeseries inputs

--- src/neurons/PoissonInput.jl
+++ src/neurons/PoissonInput.jl
@@ -1,18 +1,18 @@
 # Rates are in Hz
 maxrate = 20.
 minrate = 0.
-period = 1000. # Milliseconds
+sampleperiod = 100. # Milliseconds
 
 struct PoissonInpPopulation{T<:AbstractFloat, S<:Spike} <: InputPopulation
     id::Int
     maxrate::T
     minrate::T
-    period::T
+    sampleperiod::T
     spiketype::UnionAll
     out_spikes::Queue{S}
     length::Int
 
-    function PoissonInpPopulation(id, sz, spiketype, maxrate=maxrate, minrate=minrate, period=period)
+    function PoissonInpPopulation(id, sz, spiketype; maxrate=maxrate, minrate=minrate, sampleperiod=sampleperiod)
         (id < 1 || sz < 1) ? error("id and sz must be > 0") : nothing
 
         out_spikes = Queue(spiketype)
@@ -22,15 +22,14 @@ end
 
 function generate_input(pop::PoissonInpPopulation{T}, neuron_id::Int, sensor_inp::Vector{T}, maxval::T) where T<:AbstractFloat
     rate = compute_rate.(pop, maxval, sensor_inp)
-    sampleperiod = pop.period / length(sensor_inp)
-    expacc = ExpAccumulate.(rate, sampleperiod)
+    expacc = ExpAccumulate.(rate, pop.sampleperiod)
     spikes = pop.spiketype[]
     start = 0.
     for acc in expacc
         for t in acc
             push!(spikes, pop.spiketype(pop.id, neuron_id, t + start))
         end
-        start += sampleperiod
+        start += pop.sampleperiod
     end
     spikes
 end
