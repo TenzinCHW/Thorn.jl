427e431ec39125dc2d363e341bd9a25d18a581d5
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-10-05 21:24:32 +0800

    Updated RateInput neuron to conform with API

--- src/neurons/RateInput.jl
+++ src/neurons/RateInput.jl
@@ -3,15 +3,34 @@ maxrate = 20.
 minrate = 1.
 sampleperiod = 1000. # Milliseconds
 
-struct RateInpPopulation{T<:AbstractFloat} <: InputPopulation
+struct RateInpPopulation{T<:AbstractFloat, S<:Spike} <: InputPopulation
+    id::Int
     maxrate::T
     minrate::T
     sampleperiod::T
+    spiketype::UnionAll
+    out_spikes::Queue{S}
+    length::Int
+
+    function RateInpPopulation(id, sz, spiketype; maxrate=maxrate, minrate=minrate, sampleperiod=sampleperiod)
+        (id < 1 || sz < 1) ? error("id and sz must be > 0") : nothing
+        
+        out_spikes = Queue{spiketype}
+        new{typeof{maxrate}}(id, maxrate, minrate, sampleperiod, spiketype, out_spikes, sz)
+    end
 end
 
-function generate_input(pop::RateInpPopulation, sensor_inp::T, maxval::T, spiketype) where T<:AbstractFloat
-    timespacing = 1 / compute_rate(maxval, sensor_inp)
-    numiter = Int(floor(pop.sampleperiod / timespacing))
-    spikes = [spiketype(pop.id, t * timespacing) for t in 1:numiter]
+function generate_input(pop::RateInpPopulation, neuron_id::Int, sensor_inp::Vector{T}, maxval::T) where T<:AbstractFloat
+    timespacing = 1 ./ compute_rate.(maxval, sensor_inp)
+    numiter = Int.(floor.(pop.sampleperiod ./ timespacing))
+    spikes = pop.spiketype[]
+    start = 0.
+    for (interval, numiter) in zip(timespacing, numiters)
+        for i in 1:numiter
+            push!(spikes, pop.spiketype(pop.id, neuron_id, i * interval + start))
+        end
+        start += pop.sampleperiod
+    end
+    spikes
 end
 
