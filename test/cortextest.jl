3931218e8f3f82235b35f2c1a33d7d80ef995b4d
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2019-10-22 17:36:55 +0800

    Added more conditions to cortex test

--- test/cortextest.jl
+++ test/cortextest.jl
@@ -1,8 +1,9 @@
 sz = 5
 numsample = 4
-inp_kwargs = Dict(:sampleperiod=>50) # 50 ms sample_period
+inp_kwargs = Dict(:sampleperiod=>50.) # 50 ms sample_period
 input_neuron_types = [(PoissonInpPopulation, sz, inp_kwargs)]
-neuron_types = [(LIFPopulation, sz, stdp, 0.01)]
+proc_kwargs = Dict(:τ=>20.4)
+neuron_types = [(LIFPopulation, sz, stdp, 0.01, proc_kwargs)]
 conn = [1=>2]
 spiketype = LIFSpike
 cortex = Cortex(input_neuron_types, neuron_types, conn, spiketype)
@@ -10,4 +11,6 @@ data = [rand(sz, numsample)]
 extractors = Dict("nothing"=>(c, ps)->Dict())
 spikes, _ = process_sample!(cortex, data, 1., extractors)
 @test isa(spikes, Vector{Spike})
+@test cortex.input_populations[1].sampleperiod == 50.
+@test cortex.processing_populations[1].τ == 20.4
 
