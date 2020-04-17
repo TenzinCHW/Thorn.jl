6174800c3946db5466405bce1f51ac7524f60e5a
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2020-04-17 22:45:10 +0800

    Added sign to RateInpPopulation and fixed some bugs

--- src/neurons/RateInput.jl
+++ src/neurons/RateInput.jl
@@ -11,12 +11,13 @@ struct RateInpPopulation{T<:AbstractFloat, S<:Spike} <: InputPopulation
     spiketype::UnionAll
     out_spikes::Queue{S}
     length::Int
+    sign::Int8
 
-    function RateInpPopulation(id, sz, spiketype; maxrate=maxrate, minrate=minrate, sampleperiod=sampleperiod)
+    function RateInpPopulation(id, sz, spiketype; maxrate=maxrate, minrate=minrate, sampleperiod=sampleperiod, sign=1)
         (id < 1 || sz < 1) ? error("id and sz must be > 0") : nothing
 
-        out_spikes = Queue{spiketype}
-        new{typeof{maxrate}, spiketype}(id, maxrate, minrate, sampleperiod, spiketype, out_spikes, sz)
+        out_spikes = Queue(spiketype)
+        new{typeof(maxrate), spiketype}(id, maxrate, minrate, sampleperiod, spiketype, out_spikes, sz, sign)
     end
 end
 
@@ -26,8 +27,10 @@ function generate_input(pop::RateInpPopulation, neuron_id::Int, sensor_inp::Vect
     spikes = pop.spiketype[]
     start = 0.
     for (interval, numiter) in zip(timespacing, numiters)
-        for i in 1:numiter
-            push!(spikes, pop.spiketype(pop.id, neuron_id, i * abs(interval) + start, sign(interval)))
+        if sign(interval) == pop.sign
+            for i in 1:numiter
+                push!(spikes, pop.spiketype(pop.id, neuron_id, i * abs(interval) + start, sign(interval)))
+            end
         end
         start += pop.sampleperiod
     end
